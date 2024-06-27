58aad5e651b342183b66039e660211e8d897c938
ramaID <rama@qisthi.dev>
2024-06-28 00:01:22 +0700

    style: format PHP, JS, TS, and TSX source code

--- resources/scripts/app.tsx
+++ resources/scripts/app.tsx
@@ -1,23 +1,23 @@
-import '../css/app.css';
+import '../css/app.css'
 
-import { createRoot, hydrateRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createInertiaApp } from '@inertiajs/react'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`../views/pages/${name}.tsx`, import.meta.glob('../views/pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        if (import.meta.env.DEV) {
-            createRoot(el).render(<App {...props} />);
-            return
-        }
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`../views/pages/${name}.tsx`, import.meta.glob('../views/pages/**/*.tsx')),
+  setup({ el, App, props }) {
+    if (import.meta.env.DEV) {
+      createRoot(el).render(<App {...props} />)
+      return
+    }
 
-        hydrateRoot(el, <App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+    hydrateRoot(el, <App {...props} />)
+  },
+  progress: {
+    color: '#4B5563',
+  },
+})
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '~': '/resources',
-        },
+  resolve: {
+    alias: {
+      '~': '/resources',
     },
-    plugins: [
-        laravel({
-            input: 'resources/scripts/app.tsx',
-            ssr: 'resources/scripts/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-});
+  },
+  plugins: [
+    laravel({
+      input: 'resources/scripts/app.tsx',
+      ssr: 'resources/scripts/ssr.tsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+})
